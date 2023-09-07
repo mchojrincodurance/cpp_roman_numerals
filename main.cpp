df3fa19a720c10b7726e30f07455bb61dbded850
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-09-07 14:11:55 +0200

    Passing test with fake implementation

--- main.cpp
+++ main.cpp
@@ -4,15 +4,24 @@
 using namespace std;
 
 string convert(int arabic) {
-    return "";
+    if (arabic == 2) {
+        return "II";
+    }
+
+    return "I";
 }
 
-TEST_P(RomanNumeralsSuite, convert_arabic_numbers_into_roman_numerals)
-{
-    std::tuple<int, std::string> param = GetParam();
-    EXPECT_EQ(convert(std::get<0>(param)), std::get<1>(param));
+TEST_P(RomanNumeralsSuite, convert_arabic_numbers_into_roman_numerals) {
+    tuple<int, string> param = GetParam();
+    EXPECT_EQ(convert(get<0>(param)), get<1>(param)) << "I was expecting '" << get<1>(param) << "' but got '" << convert(get<0>(param)) << "' instead";
 }
 
-INSTANTIATE_TEST_SUITE_P(MeenyMinyMoe,
-                         RomanNumeralsSuite,
-                         testing::Values(std::make_tuple(1, "I")));
+INSTANTIATE_TEST_SUITE_P(
+        MeenyMinyMoe,
+        RomanNumeralsSuite,
+        testing::Values(
+                make_tuple(1, "I")
+                ,make_tuple(2, "II")
+//                ,make_tuple(3, "III")
+        )
+);
